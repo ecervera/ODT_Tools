1eae51451586bc218bf7d4c24aa637f40ea90b20
Enric Cervera <ecervera@uji.es>
2017-10-19 14:35:51 +0200

    web example

---
+++ web/example/tornadofileupload.py
@@ -0,0 +1,35 @@
+import tornado
+import tornado.ioloop
+import tornado.web
+import os, uuid
+
+__UPLOADS__ = "uploads/"
+
+class Userform(tornado.web.RequestHandler):
+    def get(self):
+        self.render("fileuploadform.html")
+
+
+class Upload(tornado.web.RequestHandler):
+    def post(self):
+        try:
+            fileinfo = self.request.files['filearg'][0]
+            fname = fileinfo['filename']
+            extn = os.path.splitext(fname)[1]
+            cname = str(uuid.uuid4()) + extn
+            fh = open(__UPLOADS__ + cname, 'wb')
+            fh.write(fileinfo['body'])
+            self.finish(cname + " is uploaded!! Check %s folder" %__UPLOADS__)
+        except KeyError:
+            self.finish("No s'ha triat cap fitxer.")
+
+
+application = tornado.web.Application([
+        (r"/", Userform),
+        (r"/upload", Upload),
+        ], debug=True)
+
+
+if __name__ == "__main__":
+    application.listen(8888)
+    tornado.ioloop.IOLoop.instance().start()
